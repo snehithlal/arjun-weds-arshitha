18b5c163047ccdd250a2bceab85c7ec0b12edfcc
snehithlal <pssnehithlal@gmail.com>
2026-08-10 09:42:21 +0530

    Fix the images

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,14 +1,23 @@
-export const heroPhoto: string | undefined = '/images/couple.jpg'
-export const groomPhoto: string | undefined = '/images/groom.jpg'
-export const bridePhoto: string | undefined = '/images/bride.jpg'
+import groom from '../assets/images/groom.jpg'
+import bride from '../assets/images/bride.jpg'
+import couple from '../assets/images/couple.jpg'
+import gallery1 from '../assets/images/gallery-1.jpg'
+import gallery2 from '../assets/images/gallery-2.jpg'
+import gallery3 from '../assets/images/gallery-3.jpg'
+import gallery4 from '../assets/images/gallery-4.jpg'
+import gallery5 from '../assets/images/gallery-5.jpg'
+import gallery6 from '../assets/images/gallery-6.jpg'
+
+export const heroPhoto: string | undefined = couple
+export const groomPhoto: string | undefined = groom
+export const bridePhoto: string | undefined = bride
 
 export const galleryPhotos: string[] = [
-  '/images/couple.jpg',
-  '/images/gallery-1.jpg',
-  '/images/gallery-2.jpg',
-  '/images/gallery-3.jpg',
-  '/images/gallery-4.jpg',
-  '/images/gallery-5.jpg',
-  '/images/gallery-6.jpg',
+  couple,
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+  gallery5,
+  gallery6,
 ]
-
